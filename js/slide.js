be03dd96f66bac9e0ba2a63827930cac59cdd072
dan2unni3 <76501248+dan2unni3@users.noreply.github.com>
2021-01-15 21:55:40 +0900

    210115

--- js/slide.js
+++ js/slide.js
@@ -2,11 +2,13 @@ $(function(){
     $(".lazy").slick({
         lazyLoad: 'ondemand', // ondemand progressive anticipated
         infinite: true,
-        dots:true
+        dots:true,
+        autoplay : true,			// 자동 스크롤 사용 여부
+        autoplaySpeed : 3000 		// 자동 스크롤 시 다음으로 넘어가는데 걸리는 시간 (ms)
       });
 
       $(".regular").slick({
-        dots: false, // 페이저 끄기
+        dots: true, // 페이저 끄기
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1
